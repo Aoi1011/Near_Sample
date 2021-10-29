2d3bed3374ebb4c6796d445d8215d2b4b23824bb
Aoi <ht.so.ik0102@gmail.com>
2021-10-29 07:42:13 +0700

    feat: 🎸 contract
    Create new contract

--- ft/src/lib.rs
+++ ft/src/lib.rs
@@ -1,3 +1,21 @@
+use near_contract_standards::fungible_token::metadata::{
+    FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC,
+};
+use near_contract_standards::fungible_token::FungibleToken;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::json_types::{ValidAccountId, U128};
+use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue};
+
+near_sdk::setup_alloc!();
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    token: FungibleToken, 
+    metadata: LazyOption<FungibleTokenMetadata>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
