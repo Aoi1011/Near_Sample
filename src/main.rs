5d1dd0f64c46baae4216ec88bb00d5b34d9ba7c4
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-12 11:12:05 +0700

    Success to connect with chin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bitcoincore_rpc::{Auth, Client, RpcApi};
+
 fn main() {
-    println!("Hello, world!");
+    let rpc = Client::new(
+        "http://127.0.0.1:18332",
+        Auth::UserPass("alice".to_string(), "alicepassword".to_string()),
+    )
+    .unwrap();
+
+    let best_block_hash = rpc.get_best_block_hash().unwrap();
+    println!("best block hash: {best_block_hash}");
 }
