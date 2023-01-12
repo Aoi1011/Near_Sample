29733c338a9904b6e33fa81479eff42c02b737e6
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-12 11:13:36 +0700

    Add new function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 
 fn main() {
+    rpc_example();
+}
+
+fn rpc_example() {
     let rpc = Client::new(
         "http://127.0.0.1:18332",
         Auth::UserPass("alice".to_string(), "alicepassword".to_string()),
