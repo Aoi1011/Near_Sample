23ab8b0294796143d85dd4324bde8ee285f209a7
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-12 11:43:40 +0700

    Get block info

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,6 @@ fn rpc_example() {
     )
     .unwrap();
 
-    let best_block_hash = rpc.get_best_block_hash().unwrap();
-    println!("best block hash: {best_block_hash}");
+    let best_block_hash = rpc.get_blockchain_info().unwrap();
+    println!("best block hash: {}", best_block_hash.blocks);
 }
