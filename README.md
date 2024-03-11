a3f337550ec226190f120cbc68e14430cc627c64
aoikurokawa <ht.so.ik0102@gmail.com>
2024-03-11 09:00:07 +0900

    feat: vote accounts

--- README.md
+++ README.md
@@ -26,6 +26,26 @@ curl "https://mainnet.helius-rpc.com/?api-key=ff28efe6-4fe6-4cf5-9525-01adeed6ee
                               ' | jq
 ```
 
+Prover/Verifier model
+The full nodes are trying to convince the light client of the system's state. In this context, the light client is known as the verifier and the full nodes  are known as the provers. 
+
+
+Assumptions
+
+
+The PoPos Primitive
+
+
+## The Optimistic Light Client
+
+
+## The Superlight Client
+
+
+Merkle mountain ranges
+
+
+
 ## What is RPC?
 RPC stands for Retemo Procedure Call. These are nodes that participate in the blockchain network and expose methods.
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use anyhow::Context;
 use solana_client::nonblocking::rpc_client::RpcClient;
 
 #[tokio::main]
-async fn main() {
-    let url = "http://localhost:8899".to_string();
+async fn main() -> anyhow::Result<()> {
+    let url = "".to_string();
     let rpc_client = RpcClient::new(url);
-    let block = rpc_client.get_block(1).await.expect("get block");
+    // let block = rpc_client.get_block(1).await.expect("get block");
 
-    eprintln!("Block: {:?}", block);
+    let vote_accounts = rpc_client
+        .get_vote_accounts()
+        .await
+        .context("get vote accounts")?;
+
+    eprintln!("Vote accounts: {}", vote_accounts.current.len());
+
+
+
+    // eprintln!("Block: {:?}", block);
+
+    Ok(())
 }
